bdf181e9ef97485408c04ae56e1e9f7de5f04856
Leticia Rodriguez <lrodriguez@codigodelsur.com>
2021-07-15 09:57:07 -0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Leticia Rodriguez on 7/15/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
